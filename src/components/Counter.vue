fb7a1b0c2d149ae87fd75690c46c14ceef41285e
Ushinji <siguma971@gmail.com>
2018-12-30 14:02:30 +0900

    vue init

---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hogeカウンター</h1>
+    <div>{{ count }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
