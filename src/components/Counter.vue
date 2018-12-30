1cc1158ec708d7a624a5271360413d25df339ddd
Ushinji <siguma971@gmail.com>
2018-12-30 14:26:38 +0900

    onClickメソッドを実装

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Hogeカウンター</h1>
     <div>{{ count }}</div>
+    <button v-on:click="handleClick">+</button>
   </div>
 </template>
 
@@ -12,6 +13,11 @@ export default {
     return {
       count: 0,
     };
+  },
+  methods: {
+    handleClick: () => {
+      alert("クリックが押されました!");
+    },
   },
 };
 </script>
